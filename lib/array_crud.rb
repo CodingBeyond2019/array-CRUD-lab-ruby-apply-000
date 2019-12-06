6eeb168909493e7c62b3c628e72417a821590a69
nickie kane <kameo712@gmail.com>
2019-12-06 20:38:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << 5
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(6, 7)
 end
 
 def remove_element_from_end_of_array(array)
